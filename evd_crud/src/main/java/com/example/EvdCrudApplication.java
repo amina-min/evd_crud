15f159266ee944b45d016ac9b92d93d7dcb45259
Amina <aminaemcbd@gmail.com>
2021-12-30 00:41:29 +0600

    first commit

---
+++ evd_crud/src/main/java/com/example/EvdCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvdCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvdCrudApplication.class, args);
+	}
+
+}
